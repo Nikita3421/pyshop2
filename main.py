656cc7260a9afe837c84405c7e3399a8ca4b972a
Nekit <nikitadeltamriya@gmail.com>
2023-04-16 12:30:25 +0200

    'мощно'

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello, world!")
+from flask import Flask, request
+
+app = Flask(__name__)
+app.config.from_object('settings')
+
+@app.route('/')
+def index():
+    # some_value = request.args
+    # name = some_value.get('name')
+    # age = some_value.get('age')
+    # return "Hello, " + name + " " + age
+    return "hello world"
+
+@app.route('/feed/history')
+def history():
+    return "My name is Vova"
+
+@app.route('/test')
+def test():
+    return "Test"
+
+# app.add_url_rule('/', "index", index)
+app.run()
